2fb5b629cfea3665bf23c7133f545ff773d1eed6
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 20:44:21 +0530

    Create program1.cpp

---
+++ program1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin>>a;
+	cout<<"the entered integer is "<<a<<endl;
+	double b;
+	cin>>b;
+	cout<<"the entered number with desimal is "<<b<<endl;
+	bool c;
+	cin>>c;
+	cout<<"are you a student(1=true 0=false) "<<"the statement is "<<c<<endl;
+	char d;
+	cin>>d;
+	cout<<"the first letter of apple is "<<d<<endl;
+	return 0;
+}
