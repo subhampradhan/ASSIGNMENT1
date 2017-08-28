43b975b36aa05f8e95db4f905bc307a67e9b0d85
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 21:07:37 +0530

    Create program3.cpp

---
+++ program3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b,sum,dif,mul,div;
+	cin>>a>>b;
+	sum=a+b;
+	dif=a-b;
+	mul=a*b;
+	div=a/b;
+	cout<<"the entered two numbers are "<<a<<" "<<b<<" addition is "<<sum<<"subsrtaction is "<<dif<<"multiplication is "<<mul<<"division is "<<div<<endl;
+	return 0;
+}
