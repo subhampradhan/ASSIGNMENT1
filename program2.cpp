90aa13f0dbb0d5051850f34def93a8bee6b9ffe6
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 20:55:36 +0530

    Create program2.cpp

---
+++ program2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b,c;
+	cin>>a>>b;
+	c=a+b;
+	cout<<"the entered two numbers are "<<a<<" "<<b<<"their sum is "<<c<<endl;
+	return 0;
+}
