d14062059b1f1a0b634e351b8c07ad077cdb65ad
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 21:33:18 +0530

    Create program6.cpp

---
+++ program6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float r,d,a,c;
+	cin>>r;
+	cout<<"radius of circlr is "<<r<<endl;
+	d=2*r;
+	a=3.14*r*r;
+	c=2*3.14*r;
+	cout<<"area of circle is "<<a<<" diameter is "<<d<<" circumferenceis "<<c<<endl;
+	return 0;
+}
