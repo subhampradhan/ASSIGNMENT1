243fc99993ca46909565eca6dd38a09436b77c6c
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 21:17:21 +0530

    Create program4.cpp

---
+++ program4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b,p;
+	cin>>a>>b;
+	cout<<"length of rectangle is "<<a<<" bredth of rectangle is "<<b<<endl;
+	p=2*(a+b);
+	cout<<"perimeter of rectangle is "<<p<<endl;
+	return 0;
+}
