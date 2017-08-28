a729de9a48c7f5bbeaef79d0b43d1082a73aab08
subhampradhan <31339374+subhampradhan@users.noreply.github.com>
2017-08-28 21:20:30 +0530

    Create program5.cpp

---
+++ program5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b,p;
+	cin>>a>>b;
+	cout<<"length of rectangle is "<<a<<" bredth of rectangle is "<<b<<endl;
+	p=a*b;
+	cout<<"area of rectangle is "<<p<<endl;
+	return 0;
+}
